34d9d47874ca7c27e88ac1e70088423c84b42cc9
Sahasawat Srijamsai <kdarth666@gmail.com>
2026-02-14 17:05:36 +0700

    update dashboard and inbox layout

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,10 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from '../service/dashboard.service';
+import { registerLocaleData } from '@angular/common';
+import localeTh from '@angular/common/locales/th';
+
+registerLocaleData(localeTh);
 
 @Module({
   controllers: [DashboardController],
